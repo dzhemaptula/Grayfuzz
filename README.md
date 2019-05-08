e009764036aba39c8df1b49f8eae0a05e86c1b55
dzhemaptula <dzhem.aptula@gmail.com>
2019-05-08 17:22:27 +0200

    Error ergens- opnieuw beginnen

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Grayfuzz
+# Technora
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Grayfuzz'`, () => {
+  it(`should have as title 'Technora'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Grayfuzz');
+    expect(app.title).toEqual('Technora');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Grayfuzz!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Technora!');
   });
 });
