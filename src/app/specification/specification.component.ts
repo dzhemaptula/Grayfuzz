0a2c4ad19b8824898e8ae2fe7c7c2ea5923677f2
dzhemaptula <dzhem.aptula@gmail.com>
2019-05-08 17:31:12 +0200

    test commit

--- src/app/specification/specification.component.ts
+++ src/app/specification/specification.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SpecificationComponent implements OnInit {
 
   @Input() name: string;
-
+  //test
   constructor() { }
 
   ngOnInit() {
